717a5b5630fce957258b1a49b53252356fea1341
IsisTD <220126@fppiramide.com>
2024-02-09 14:17:40 +0100

    mn,

--- context/postgres.connector.ts
+++ context/postgres.connector.ts
@@ -1,24 +1,29 @@
-import { Pool } from 'pg';
-import dotenv from "dotenv"
-dotenv.config()
+import { Pool } from "pg";
+import dotenv from "dotenv";
+dotenv.config();
 
-const dbHost = process.env.POSTGRES_HOST 
-const dbUser = process.env.POSTGRES_USER
-const dbPassword = process.env.POSTGRES_PW
-const dbName = process.env.POSTGRES_DB
+const dbHost = process.env.POSTGRES_HOST;
+const dbUser = process.env.POSTGRES_USER;
+const dbPassword = process.env.POSTGRES_PASSWORD;
+const dbName = process.env.POSTGRES_DB;
 
 const pool = new Pool({
-    max: 20,
-    connectionString: `postgres://${dbUser}:${dbPassword}@${dbHost}:5432/${dbName}`,
-    idleTimeoutMillis: 30000
+  max: 1000,
+  connectionString: `postgres://${dbUser}:${dbPassword}@${dbHost}:5432/${dbName}`,
+  idleTimeoutMillis: 30000,
 });
 
-
-const executeQuery = async (sql: any) => {
-    const client = await pool.connect()
-    const {rows} = await client.query(sql)
-    client.release()
-    return rows
-}
+const executeQuery = async (sql: any, data?: any[]) => {
+  const client = await pool.connect();
+  try {
+    const { rows } = await client.query(sql, data);
+    client.release();
+    return rows;
+  } catch (err) {
+    console.error(err);
+    client.release();
+    return [];
+  }
+};
 
 export default executeQuery;
